5d5d41bfb45dfe89d657a2cb779fa484a5dcbf3d
Fábio José <fabiojose@gmail.com>
2022-02-24 16:21:10 -0300

    Rename package from *.kafka to *.klay
    Signed-off-by: Fábio José <fabiojose@gmail.com>

---
+++ src/main/java/com/github/fabiojose/klay/Klay.java
@@ -0,0 +1,22 @@
+package com.github.fabiojose.klay;
+
+import io.quarkus.runtime.annotations.QuarkusMain;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@QuarkusMain
+@Command(
+  name = "klay",
+  subcommands = {
+    RunCommand.class,
+    StartCommand.class
+  }
+)
+public final class Klay {
+
+  public static void main(String[] args) {
+    new CommandLine(Klay.class).execute(args);
+    //Quarkus.run(args);
+  }
+
+}

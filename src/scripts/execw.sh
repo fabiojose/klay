4adf315b068bfd199387634237babe07147004da
Fábio José <fabiojose@gmail.com>
2022-03-16 15:57:16 -0300

    fix: The command to check the status of pid
    Signed-off-by: Fábio José <fabiojose@gmail.com>

--- src/scripts/execw.sh
+++ src/scripts/execw.sh
@@ -17,7 +17,7 @@ fi
 
 if [ "$DETACH" == "n" ]; then
   # KLAY ID     LINUX PID    TYPE   VERSION    CREATED    STATUS    PORTS   ARGS
-  echo "$EXTERNAL_ID;cat '$PROCESS_DIR/pid';cat '$PROCESS_DIR/type';cat '$PROCESS_DIR/version';$(date +"%Y-%m-%d %H:%M:%S");bash psw.sh $(cat $PROCESS_DIR/pid);cat '$PROCESS_DIR/ports';'$@'" >> $KLAY_PROCESSES
+  echo "$EXTERNAL_ID;cat '$PROCESS_DIR/pid';cat '$PROCESS_DIR/type';cat '$PROCESS_DIR/version';$(date +"%Y-%m-%d %H:%M:%S");bash psw.sh \$(cat $PROCESS_DIR/pid);cat '$PROCESS_DIR/ports';'$@'" >> $KLAY_PROCESSES
 
   java $JAVA_OPTS -jar $KLAY_UBER_JAR_LOCATION --external-id=${EXTERNAL_ID} "$@"
 else

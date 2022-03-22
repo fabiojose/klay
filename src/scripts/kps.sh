fa5c6a3ed7fda515e39aa681fabd491af40c10e1
Fábio José <fabiojose@gmail.com>
2022-03-22 18:27:43 -0300

    feat: klay ps --all to list all process
    Signed-off-by: Fábio José <fabiojose@gmail.com>

--- src/scripts/kps.sh
+++ src/scripts/kps.sh
@@ -7,17 +7,19 @@ if ! [ -x "$(command -v column)" ]; then
   exit 5
 fi
 
+ALL=$([[ "$1" == "-a" ]] || [[ "$1" == "--all" ]] && echo -n 'y' || echo -n 'n')
+
 fmt="%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
-(printf "$fmt" 'KLAY-ID' "PID" "TYPE" "VERSION" "PORTS"
+(printf "$fmt" 'KLAY-ID' "PID" "TYPE" "VERSION" "PORTS" "STATUS"
 while IFS=';' read -r klayId pidSupplier typeSupplier versionSupplier created statusSupplier portsSupplier args; do
 
   pid=$(eval $pidSupplier)
   status=$(eval $statusSupplier) 2>/dev/null
-  if [ "$status" == "Running" ]; then
+  if [ "$status" == "Running" ] || [ "$ALL" == "y" ]; then
     # Get the type, version, status and ports
     type=$(eval $typeSupplier)
     version=$(eval $versionSupplier)
     ports=$(eval $portsSupplier)
-    printf "$fmt" "$klayId" "$pid" "$type" "$version" "$ports"
+    printf "$fmt" "$klayId" "$pid" "$type" "$version" "$ports" "$status"
   fi
 done < "$KLAY_PROCESSES") | column -t

d0f7f12a83419c133437c679ab9beb0b508d7645
Fábio José <fabiojose@gmail.com>
2022-03-24 16:21:39 -0300

    chore: stub of build templates
    todos to reachout the build feature
    
    Signed-off-by: Fábio José <fabiojose@gmail.com>

--- src/main/java/com/github/fabiojose/klay/streams/StreamsBuildCommand.java
+++ src/main/java/com/github/fabiojose/klay/streams/StreamsBuildCommand.java
@@ -9,8 +9,40 @@ import picocli.CommandLine.Command;
 )
 public class StreamsBuildCommand implements Runnable {
 
+  // TODO: Same parameters and options of StreamsCommand ?
+
+  // String packageName;
+
   @Override
   public void run() {
+
+    // TODO: Split klay: core and cli
+
+    // TODO: Publish to sonatype
+
+    // TODO: Needs gradle 7.4.1: download
+
+    // TODO: Classes of kay to start streams
+
+    // TODO: Dependencies needed
+
+    // TODO: Collect deps
+    // quarkus
+    // custom QuarkusMain: a java file. Template ?
+    // custom TopologyBuilder: a java file. Template ?
+
+    // TODO: Build java files
+
+    // TODO: Build uber-jar
+    //  with compiled java files. Template?
+    //  with compiled java topology, if java
+    //  with groovy file, if groovy
+
+    // TODO: Generate application.properties
+
+    // TODO: Generate Dockerfile. Template?
+
+    // TODO: Generate build-and-run.sh
 
   }
 
